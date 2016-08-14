214d6e49c16d6e82877f7829226a6af430d451ed
Kevin Lewis <kevin@jellybabi.es>
2016-08-14 20:48:12 +0100

    Site V2

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 80
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+    });
+});
